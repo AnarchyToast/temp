16488029f3e8405e9ea0cf7828225a12667f0bdb
AnarchyToast <62135418+AnarchyToast@users.noreply.github.com>
2023-08-05 15:30:53 +0000

    aaaaaa

--- prb2.py
+++ prb2.py
@@ -1,21 +1,24 @@
 def diff_check(sample, target):
     if(sample != target):
-        return(target - sample)
+        return(int(target) - int(sample))
     else:
         return(0)
 
 temp_input = input()
-input = temp_input.split()
+units = temp_input.split()
 
 diff = []
 
-for i in input:
+for i in range(0, len(units)):
+    i = int(i)
     if i == 0:
-       diff.append(diff_check(int(input[i]), 1))
+        diff.append(diff_check(units[i], 1))
     elif i == 1:
-        diff.append(diff_check(int(input[i]), 1))
-        
+        diff.append(diff_check(units[i], 1))
+    elif i == 5:
+        diff.append(diff_check(units[i], 8))
+    else:
+        diff.append(diff_check(units[i], 2))
 
-
-
-print(diff)
+for i in range(0, len(diff)):
+    print(diff[i], end = " ")
